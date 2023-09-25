01343341b269062455babb4a6c2dedfda01a35cb
kengurenokRu <nastassiadunaeva@gmail.com>
2023-09-25 15:37:48 +0300

    init: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        minRange = 0;
+        maxRange = 0;
+    }
 
     setRange(min, max) {
+        minRange = min;
+        maxRange = max;
 
     }
 
     guess() {
 
+        res = Math.floor((minRange + maxRange) / 2);
+        return res;
+
     }
 
     lower() {
+        minRange = guess();
 
     }
 
     greater() {
-
+    maxRange = guess();
     }
 }
 
